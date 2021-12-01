8f0734d33d75554c9e32cce7800fd85e8fb2bb8d
YuhuiWu92 <94534171+YuhuiWu92@users.noreply.github.com>
2021-12-02 00:28:38 +0100

    create pokemonlist, add items

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+let pokemonList = [
+    { theName: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
+    { theName: 'Golbat', height: 1.6, types: ['flying', 'poison'] },
+    { theName: 'Butterfree', height: 1.1, types: ['flying', 'bug'] },
+    { theName: 'Nidoran', height: 0.4, types: ['poison'] },
+    { theName: 'Charizard', height: 1.7, types: ['flying', 'fire'] }
+];
