d1d1a49e69525c0e70b8b2011cd5533ce568562b
YuhuiWu92 <94534171+YuhuiWu92@users.noreply.github.com>
2021-12-02 00:30:33 +0100

    change thename to name

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonList = [
-    { theName: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
-    { theName: 'Golbat', height: 1.6, types: ['flying', 'poison'] },
-    { theName: 'Butterfree', height: 1.1, types: ['flying', 'bug'] },
-    { theName: 'Nidoran', height: 0.4, types: ['poison'] },
-    { theName: 'Charizard', height: 1.7, types: ['flying', 'fire'] }
+    { Name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
+    { Name: 'Golbat', height: 1.6, types: ['flying', 'poison'] },
+    { Name: 'Butterfree', height: 1.1, types: ['flying', 'bug'] },
+    { Name: 'Nidoran', height: 0.4, types: ['poison'] },
+    { Name: 'Charizard', height: 1.7, types: ['flying', 'fire'] }
 ];
