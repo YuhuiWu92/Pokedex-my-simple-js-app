0f8a4bd8b6716c74086b9ac2f5cf20e576102b0c
YuhuiWu92 <94534171+YuhuiWu92@users.noreply.github.com>
2021-12-06 10:19:46 +0100

    set for-loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 let pokemonList = [
-    { Name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
-    { Name: 'Golbat', height: 1.6, types: ['flying', 'poison'] },
-    { Name: 'Butterfree', height: 1.1, types: ['flying', 'bug'] },
-    { Name: 'Nidoran', height: 0.4, types: ['poison'] },
-    { Name: 'Charizard', height: 1.7, types: ['flying', 'fire'] }
+    { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
+    { name: 'Golbat', height: 1.6, types: ['flying', 'poison'] },
+    { name: 'Butterfree', height: 1.1, types: ['flying', 'bug'] },
+    { name: 'Nidoran', height: 0.4, types: ['poison'] },
+    { name: 'Charizard', height: 1.7, types: ['flying', 'fire'] }
 ];
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write('<br>' + pokemonList[i].name + (' , height: ') + pokemonList[i].height);
+    if (pokemonList[i].height >= 2) {
+        document.write(' - Wow, that\'s big!');
+    }
+}
