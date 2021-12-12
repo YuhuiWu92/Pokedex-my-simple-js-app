17be7690ae57e9cedc607f5bc3a4565c10562640
YuhuiWu92 <94534171+YuhuiWu92@users.noreply.github.com>
2021-12-12 23:43:00 +0100

    IIFE wrap

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
-let pokemonList = [
-    { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
-    { name: 'Golbat', height: 1.6, types: ['flying', 'poison'] },
-    { name: 'Butterfree', height: 1.1, types: ['flying', 'bug'] },
-    { name: 'Nidoran', height: 0.4, types: ['poison'] },
-    { name: 'Charizard', height: 1.7, types: ['flying', 'fire'] }
-];
+//IIFE wrap
+let pokemonRepository = (function() {
+    let pokemonList = [
+        { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
+        { name: 'Golbat', height: 1.6, types: ['flying', 'poison'] },
+        { name: 'Butterfree', height: 1.1, types: ['flying', 'bug'] },
+        { name: 'Nidoran', height: 0.4, types: ['poison'] },
+        { name: 'Charizard', height: 1.7, types: ['flying', 'fire'] }
+    ];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
 
 // Part 1: Use a forEach() function instead of the for loop 
 
@@ -18,8 +34,11 @@ function myPokemonPrintout(pokemon) {
 }
 
 document.write('<ul>');
-//Call forEach function to show Pokemon list
-pokemonList.forEach(myPokemonPrintout);
-document.write('</ul>');
 
-// Part 2: IIFE
+// add pokemon
+pokemonRepository.add({ name: 'Pikachu', height: 0.4, type: ['electric'] });
+
+//Call forEach function to show Pokemon list
+pokemonRepository.getAll().forEach(myPokemonPrintout);
+
+document.write('</ul>');
